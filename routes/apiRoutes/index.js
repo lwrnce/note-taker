c329fd327f86da195356c54f01067eec03f194b5
lwrnce <lwrnceyuen@gmail.com>
2021-05-23 01:05:25 -0700

    fix pathing

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,21 +4,38 @@ const path = require('path');
 const uuid = require('uuid');
 let { notes } = require('../../db/db.json');
 
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
 router.get('/notes/:id', (req, res) => {
     const noteData = notes.filter(note => note.id === parseInt(req.params.id));
     if (!noteData) {
-        res.status(204).send("There are no notes!");
-    } else {
-       res.send(noteData);
+        throw new Error("You don't have any notes!");
+    } 
+    return res.send(noteData);
+});
+
+router.post('/notes', (req, res) =>{
+    res.send(req.body);
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid.v4()
     }
+    if (!newNote.title || !newNote.text) {
+        throw new Error('Please enter a title and some text for your note.');
+    }
+    notes.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify({ notes }, null, '\t')
+    );
+    return newNote;
 });
 
-router.post((req, res) =>{
+// router.delete((req, res) => {
 
-});
-
-router.delete((req, res) => {
-
-});
+// });
 
 module.exports = router;
