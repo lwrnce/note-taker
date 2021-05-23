8fcab18f6a6cd4a4bc8a90ad65dd5ccff6f8b785
lwrnce <lwrnceyuen@gmail.com>
2021-05-22 23:45:46 -0700

    update api routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,26 +1,24 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
+let { notes } = require('../../db/db.json');
 
-const save = require('../../public/assets/js/save');
-
-router.get('/notes', (req, res) => {
-    save
-        .grabNotes()
-        .then(notes => res.json(notes))
-        .catch(err => res.status(500).json(err));
+router.get('/notes/:id', (req, res) => {
+    const noteData = notes.filter(note => note.id === parseInt(req.params.id));
+    if (!noteData) {
+        res.status(204).send("There are no notes!");
+    } else {
+       res.send(noteData);
+    }
 });
 
-router.post('/notes', (req, res) => {
-    save
-        .addNote(req.body)
-        .then((note) => res.json(note))
-        .catch(err => res.status(500).json(err));
+router.post((req, res) =>{
+
 });
 
-router.delete('/notes/:id', function (req, res) {
-    save
-        .deleteNote(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch(err => res.status(500).json(err));
+router.delete((req, res) => {
+
 });
 
 module.exports = router;
